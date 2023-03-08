1599be18e018e2e3132257ee2714ce11dca3055b
M. Ivkovic <m.ivkovic.dev@gmail.com>
2023-03-08 11:48:09 +0000

    edit todo

--- middlewear-app/routes/todos.js
+++ middlewear-app/routes/todos.js
@@ -42,5 +42,32 @@ router.post("/", checkToken, checkContentType, checkDescription, (req, res) => {
   res.status(200).json({ message: "Todo added" });
 });
 
+// PUT request to update a todo
+// takes id from url, and new todo data from body
+router.put(
+  "/:id",
+  checkToken,
+  checkContentType,
+  checkDescription,
+  (req, res) => {
+    // load todos.json file from local directory
+    let todos = getTodosData();
+    // find the todo in the todos array, get id from req.params, and update it
+    let todo = todos.find((todo) => todo.id === parseInt(req.params.id));
+    // if todo is found, update it
+    if (todo) {
+      todo.title = req.body.title;
+      todo.description = req.body.description;
+      todo.completed = req.body.completed;
+      // update todos.json file
+      fs.writeFileSync("./database/todos.json", JSON.stringify(todos));
+      // send response to client
+      res.status(200).json({ message: "Todo updated" });
+    } else {
+      res.status(404).json({ message: "Todo not found" });
+    }
+  }
+);
+
 //export router
 module.exports = router;
