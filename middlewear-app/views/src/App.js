75b95651bf13861fb00af29ccf801da3bd7f5110
M. Ivkovic <m.ivkovic.dev@gmail.com>
2023-03-09 06:50:47 +0000

    framework & flow plan

--- middlewear-app/views/src/App.js
+++ middlewear-app/views/src/App.js
@@ -11,12 +11,13 @@ function App() {
   // conditional rendering depednig on if user is logged in or not (stored in state isLoggedIn)
   // if user is not logged in, display log in and sign up forms
   // if user is logged in, display that users todo list
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>React App</h1>
+        <h1>enlisted</h1>
+        {isLoggedIn ? <button>Log Out</button> : null}
       </header>
       <main>{isLoggedIn ? <TodoList /> : <Welcome />}</main>
     </div>
--- middlewear-app/views/src/components/Welcome.js
+++ middlewear-app/views/src/components/Welcome.js
@@ -5,6 +5,8 @@
 
 // need to think abou this to get correct rendering. On load should be two buttons. Then once a button is clicked the correct version of the form will be rendered. Need to use state to determine what to render
 
+// rethink, just have a username / password form with two buttons, log in and register. If log in is clicked, send a post request to the server to log in. If register is clicked, send a post request to the server to register a new user
+
 function Welcome(props) {
   return (
     <div className="welcome">
